correct playresult calculation for duplicates

mastermind_final.py
@@ -16,20 +16,31 @@ def givenum(num_colors, duplicate=False):
 # Returns the result of the number of A and B
 # A = Correct Color + Correct position
 # B = Correct Color + wrong position
+# Changed logic of calculation to factor in for duplicate scenario not originally present in the code 
 def playresult(notknow, guess):
     A = 0
     B = 0
-    for idx, val in enumerate(notknow):
-        for idx2, val2 in enumerate(guess):
-            if (idx == idx2 and val == val2):  # position & value are correct
-                A = A + 1
-            elif (val == val2):
-                B = B + 1
+    temp_notknow = []
+    temp_guess = []
+    
+    for i in range(len(notknow)): # checking for blacks only, and remove them from the list
+        if notknow[i] == guess[i]:
+            A = A + 1      
+        else:
+            temp_notknow.append(notknow[i])
+            temp_guess.append(guess[i])
+    for guess in temp_guess:
+        if guess in temp_notknow:
+            B = B + 1 
+            idx = temp_notknow.index(guess)
+            del temp_notknow[idx]
+
     return A, B
 
 
 def chooseone(code_set):
     remain_table = np.zeros(len(code_set))
+    # print(remain_table)
     for idx, val in enumerate(code_set):
         code_idx = [j for j in range(len(code_set))]
         code_idx.remove(idx)
@@ -42,12 +53,14 @@ def chooseone(code_set):
         remain = 0
         for idxx in S:   #  each idxx acts like answer, we play it out again and see how many remaining eligible one remains
             A, B = playresult(code_set[idxx], code_set[idx])
+            print(code_set[idxx], code_set[idx], A, B)
             for k in S:
                 a, b = playresult(code_set[k], code_set[idx])
                 if (a == A and b == B):
                     remain = remain + 1
         #stores the number of remaining combinations for each c in S
         remain_table[idx] = remain
+
     #returns the index of the lowest value in remain_table
     mindex = np.argmin(remain_table)
     # the one with least average remaining eligible code is selected from the original codeset
@@ -59,6 +72,7 @@ def ini_population(num_colors,duplicate=False):
         population = itertools.product(list(range(0,num_colors)), repeat=4)
     else:
         population = permutations(list(range(0,num_colors)),4)
+
     return list(population)
 
 def format_state_list(state_list):
@@ -83,9 +97,6 @@ def format_state_list(state_list):
 # duplicate: boolean
 # num_colors: integer
 def start(code, duplicate=False, num_colors=5 ):
-    duplicate = True #decides if we want to work with duplicates
-    num_colors = 5
-
     guess_list=[]
     state_list=[]
 
@@ -93,7 +104,6 @@ def start(code, duplicate=False, num_colors=5 ):
 
     # Create a first guess randomly
     guess = givenum(num_colors,duplicate)
-
 
     # Get the feedback value with guess and code
     A, B = playresult(code, guess) 
@@ -111,8 +121,7 @@ def start(code, duplicate=False, num_colors=5 ):
         A, B = playresult(code, guess)     
         guess_list.append(guess)
         state_list.append((A,B))
-        
+            
     state_list = format_state_list(state_list)
     return guess_list, state_list 
-
 