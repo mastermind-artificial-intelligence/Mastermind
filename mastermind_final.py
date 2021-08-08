90e9917a84d24fd07ca69e36245fa0861b63beb2
Sean <seanlewtengsiong@gmail.com>
2021-08-09 00:52:01 +0800

    added detailed code function documentation

--- mastermind_final.py
+++ mastermind_final.py
@@ -26,6 +26,7 @@ COLORS_INDEX = {
 
 # For our permutation we reduce from 9 to 6 "colors" representede by digits and no duplicates
 def givenum(num_colors, duplicate=False):
+    """return a random combination from the full codeset"""
     if duplicate:
         num = np.random.choice(range(0,num_colors), 4, replace=True)
     else:
@@ -36,37 +37,39 @@ def givenum(num_colors, duplicate=False):
 # A = Correct Color + Correct position
 # B = Correct Color + wrong position
 # Changed logic of calculation to factor in for duplicate scenario not originally present in the code 
-def playresult(notknow, guess):
+def playresult(secretcode, guess):
+    """function to return feedback for each guess made"""
     A = 0
     B = 0
-    temp_notknow = []
+    temp_secretcode = []
     temp_guess = []
     
-    for i in range(len(notknow)): # checking for blacks only, and remove them from the list
-        if notknow[i] == guess[i]:
+    for i in range(len(secretcode)): # checking for blacks only, and remove them from the list
+        if secretcode[i] == guess[i]:
             A = A + 1      
         else:
-            temp_notknow.append(notknow[i])
+            temp_secretcode.append(secretcode[i])
             temp_guess.append(guess[i])
     for guess in temp_guess: #checking for whites only
-        if guess in temp_notknow:
+        if guess in temp_secretcode:
             B = B + 1 
-            idx = temp_notknow.index(guess)
-            del temp_notknow[idx]
+            idx = temp_secretcode.index(guess)
+            del temp_secretcode[idx]
 
     return A, B
 
 
 def chooseone(code_set):
+    """runs a look forward process based on referenced research paper to find the best guess that can reduce code set the most"""
     remain_table = np.zeros(len(code_set))
-    # print(remain_table)
+    
     for idx, val in enumerate(code_set):
         code_idx = [j for j in range(len(code_set))]
         code_idx.remove(idx)
         # if there is more than 100 combinations in the code set, we randomly sample 100
         if (len(code_idx) > 100):
             S = random.sample(code_idx, 100)
-        # else we take all?
+        # else we take all
         else:
             S = random.sample(code_idx, len(code_idx))
         remain = 0
@@ -80,13 +83,14 @@ def chooseone(code_set):
         #stores the number of remaining combinations for each c in S
         remain_table[idx] = remain
 
-    #returns the index of the lowest value in remain_table
+    #returns the index of the lowest value in remain_table, this is the most optimal next guess
     mindex = np.argmin(remain_table)
     # the one with least average remaining eligible code is selected from the original codeset
     return list(code_set[mindex])
 
-#initialize the code set from with all the possible permutations with 6 different "colors"
+
 def ini_population(num_colors,duplicate=False):
+    """initialize the code set from with all the possible permutations with 6 different "colors""""
     if duplicate:
         population = itertools.product(list(range(0,num_colors)), repeat=4)
     else:
@@ -95,6 +99,7 @@ def ini_population(num_colors,duplicate=False):
     return list(population)
 
 def format_state_list(state_list):
+    """format state_list to an output appropriate for frontend GUI"""
     formatted_state_list = []
 
     for each_state in state_list:
@@ -113,6 +118,7 @@ def format_state_list(state_list):
     return formatted_state_list
 
 def format_guess_list(guess_list):
+    """format guess_list to an output appropriate for frontend GUI"""
     return [[INDEX_COLORS.get(index) for index in guess] for guess in guess_list]
 
 #####################################################
@@ -120,6 +126,7 @@ def format_guess_list(guess_list):
 # duplicate: boolean
 # num_colors: integer
 def start(code, duplicate=False, num_colors=5 ):
+    """function for frontend to call for gameplay against qlearning agent"""
     guess_list=[]
     state_list=[]
 
@@ -136,7 +143,7 @@ def start(code, duplicate=False, num_colors=5 ):
     guess_list.append(guess)
     state_list.append((A,B))
 
-    while (A < 4):  # Still cleaning the code_set until we find the real answer
+    while (A < 4):  # Do until guess is correct
         play_count = play_count + 1
         code_set = [t for t in code_set if playresult(t, guess) == (A, B)]
 
